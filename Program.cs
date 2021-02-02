ccac1ef46e1d8da647a11eac2ea836903fe8ee47
Алексей Федоров <afaafa2001@mail.ru>
2021-02-02 23:27:45 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FedorovAFA.Lerning
+{   
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int Count;
+
+            List<int> list = new List<int>();
+            
+            list.Add(Convert.ToInt32( Console.ReadLine()));
+            list.Add(Convert.ToInt32(Console.ReadLine())); 
+            list.Add(Convert.ToInt32(Console.ReadLine()));
+            list.Add(Convert.ToInt32(Console.ReadLine()));
+            Count  = list.Count;
+            Console.WriteLine(Count);
+            for (int i = 0; list.Count <i ; ++)
+            {
+                 
+            }
+        }
+        static void ConsoleClear()
+        {
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
