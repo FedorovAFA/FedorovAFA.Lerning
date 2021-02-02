9000ba383f58fec0db01505ce52990c901c971ca
Алексей Федоров <afaafa2001@mail.ru>
2021-02-03 00:06:00 +0700

    Поправка в выводе на консоль

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace FedorovAFA.Lerning
         static void Main(string[] args)
         {
             int  o = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("======================================");
             Console.WriteLine(o);
             ConsoleClear();
         }
