1409b5ec5480d9292a7f16e7cb7946626ce44d78
Алексей Федоров <afaafa2001@mail.ru>
2021-02-07 22:42:12 +0700

    Try

--- Footwear.cs
+++ Footwear.cs
@@ -7,14 +7,14 @@ namespace FedorovAFA.Lerning
         public int Size { get; set; }
         public string Production { get; set; }
         public decimal Price { get; set; }
-        public Footwear(int size, string production, decimal Price) 
+        public Footwear(int size, string production, decimal Price)
         {
             this.Price = Price;
             this.Size = size;
             this.Production = production;
 
         }
-        public virtual void PrintSizeProduction() 
+        public virtual void PrintSizeProduction()
         {
             Console.WriteLine($"Размер:{Size} \nПроизводитель: {Production} \nЦена:{Price}$");
         }
--- Program.cs
+++ Program.cs
@@ -1,26 +1,26 @@
 ﻿using System;
 
 namespace FedorovAFA.Lerning
-{   
+{
     public class Program
     {
 
 
-    // TODO:  такие коментарии мо
+        // TODO:  такие коментарии мо
 
 
-    static void Main()
+        static void Main()
         {
             Console.WriteLine("                                 Нажмите Enter для начала работы");
             ConsoleClear();
-            var ListCheck = new Check(0,0);
+            var ListCheck = new Check(0, 0);
 
-            Sneakers ModelOne = new Sneakers(42, "Китай", "Бега", 3434,"Boost");
-            var sum = ListCheck + ModelOne;            
+            Sneakers ModelOne = new Sneakers(42, "Китай", "Бега", 3434, "Boost");
+            var sum = ListCheck + ModelOne;
             Console.WriteLine(sum);
-            
-            Sneakers ModelTwo = new Sneakers(36, "Тайвань", "Деловые", 231, "Mister BIN");                        
-            sum = sum + ModelTwo;
+
+            Sneakers ModelTwo = new Sneakers(36, "Тайвань", "Деловые", 231, "Mister BIN");
+            sum += ModelTwo;
             Console.WriteLine(sum);
             ConsoleClear();
         }
